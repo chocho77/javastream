2652a31101a6f9edd1315c08e8e4d6d0875bd653
chocho77 <chavmom@gmail.com>
2022-09-05 15:41:32 +0300

    Update Person.java

--- jstream/src/resource/Person.java
+++ jstream/src/resource/Person.java
@@ -1,13 +1,34 @@
 package resource;
 
 public class Person {
-    private String name;
-    private int age;
-    private Gender gender;
+    private final String name;
+    private final int age;
+    private final Gender gender;
 
     public Person(String name, int age, Gender gender) {
         this.name = name;
         this.age = age;
         this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
     }
 }
