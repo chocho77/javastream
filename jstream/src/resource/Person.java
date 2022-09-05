8d653c38d4b05a597ab48ead039b2b9b23694da2
chocho77 <chavmom@gmail.com>
2022-09-05 15:25:26 +0300

    add class

--- jstream/src/resource/Person.java
+++ jstream/src/resource/Person.java
@@ -1,5 +1,13 @@
 package resource;
 
 public class Person {
+    private String name;
+    private int age;
+    private String gender;
 
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
