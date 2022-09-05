d63ffe81ca34f0259d2e6c0313b6b058b8cf6b20
chocho77 <chavmom@gmail.com>
2022-09-05 15:32:19 +0300

    add some code

--- jstream/src/main/MainStream.java
+++ jstream/src/main/MainStream.java
@@ -1,7 +1,23 @@
 package main;
 
+import resource.Gender;
+import resource.Person;
+
+import java.util.List;
+
 public class MainStream {
     public static void main(String[] args) {
-        
+        List<Person> people = getPeople();
+
     }
+
+    private static List<Person> getPeople() {
+        return List.of(
+                new Person("James Bond", 20, Gender.MALE),
+                new Person("Alina Smith", 33,Gender.FEMALE)
+        );
+
+    }
+
+
 }
--- jstream/src/resource/Person.java
+++ jstream/src/resource/Person.java
@@ -3,9 +3,9 @@ package resource;
 public class Person {
     private String name;
     private int age;
-    private String gender;
+    private Gender gender;
 
-    public Person(String name, int age, String gender) {
+    public Person(String name, int age, Gender gender) {
         this.name = name;
         this.age = age;
         this.gender = gender;
