7c797e22f22a20654e87282fe7f28a5922185f64
chocho77 <chavmom@gmail.com>
2022-09-05 15:36:59 +0300

    Update MainStream.java

--- jstream/src/main/MainStream.java
+++ jstream/src/main/MainStream.java
@@ -14,7 +14,12 @@ public class MainStream {
     private static List<Person> getPeople() {
         return List.of(
                 new Person("James Bond", 20, Gender.MALE),
-                new Person("Alina Smith", 33,Gender.FEMALE)
+                new Person("Alina Smith", 33,Gender.FEMALE),
+                new Person("Helen White", 57,Gender.FEMALE),
+                new Person("Alex Boz", 14,Gender.MALE),
+                new Person("Jemie Goa", 99,Gender.MALE),
+                new Person("Anna Cock", 7,Gender.FEMALE),
+                new Person("Zelda Brown", 120,Gender.FEMALE)
         );
 
     }
