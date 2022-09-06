fe0bde08e5bf78daba2c946ea0fa6c163fbb724c
chocho77 <chavmom@gmail.com>
2022-09-06 12:28:52 +0300

    Update MainStream.java

--- jstream/src/main/MainStream.java
+++ jstream/src/main/MainStream.java
@@ -35,7 +35,7 @@ public class MainStream {
 
         // Sort
         List<Person> sorted = people.stream()
-                .sorted(Comparator.comparing(Person::getAge))
+                .sorted(Comparator.comparing(Person::getAge).reversed())
                 .collect(Collectors.toList());
         sorted.forEach(System.out::println);
         // All match
